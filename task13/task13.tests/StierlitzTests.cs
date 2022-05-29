d7db647655a337857dde611ab4a425a4a9da7cee
Seleznev.Semen@urfu.me <Seleznev.Semen@urfu.me>
2022-05-29 22:25:18 +0500

    task13

---
+++ task13/task13.tests/StierlitzTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task13;
+using NUnit.Framework;
+
+namespace task13.tests
+{
+    [TestFixture]
+    public class StierlitzTests
+    {
+        [Test]
+        public void EncryptionDecryptionTest()
+        {
+            var test1 = TestInput1();
+            var test2 = TestInput2();
+
+            Assert.AreEqual(test1.Encrypt().Decrypt().Message, "stierlitz");
+            Assert.AreEqual(test2.Encrypt().Decrypt().Message, "alitlebe");
+        }
+
+        private Stierlitz TestInput1()
+        {
+            return new Stierlitz("stierlitz");
+        }
+
+        private Stierlitz TestInput2()
+        {
+            return new Stierlitz("A little bee");
+        }
+    }
+}
---
+++ task13/task13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task13
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение для штаба:");
+
+            var stierlitzInput = new Stierlitz(Console.ReadLine());
+            var stierlitzCypher = stierlitzInput.Encrypt();
+            var stierlitzOutput = stierlitzCypher.Decrypt();
+
+            Console.WriteLine(stierlitzCypher.Message);
+            Console.WriteLine(stierlitzOutput.Message);
+            
+            Console.ReadKey();
+        }
+    }
+}
