f3c6657d4a1af094b970f42c953ebbb4c779a588
Seleznev.Semen@urfu.me <Seleznev.Semen@urfu.me>
2022-06-15 21:55:23 +0500

    task11

---
+++ Library/Library.Tests/ReaderUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+using NUnit.Framework;
+
+namespace Library.Tests
+{
+    [TestFixture]
+    public class ReaderUnitTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var hisao = CreateTestReader();
+
+            Assert.AreEqual("Ханако", hisao.Name);
+            Assert.AreEqual("Икедзава", hisao.Surname);
+            Assert.AreEqual(717171, hisao.Number);
+        }
+
+        [Test]
+        public void TimeTest()
+        {
+            var hisao = CreateTestReader();
+
+            hisao.StartDate = new DateTime(2012, 1, 4);
+            hisao.Span = new TimeSpan(7, 0, 0, 0);
+
+            Assert.AreEqual(hisao.EndDate, new DateTime(2012, 1, 11));
+        }
+
+        [Test]
+        public void ToStringTest()
+        {
+            var hisao = CreateTestReader();
+            Assert.AreEqual("Хисао Накай", hisao.ToString());
+        }
+
+        private Reader CreateTestReader()
+        {
+            return new Reader("Хисао", "Накай", 717171);
+        }
+
+    }
+}
---
+++ Library/Library/Reader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Reader
+    {
+        public string Name;
+        public string Surname;
+        public readonly int Number;
+
+        public List<string> Literature;
+        public DateTime StartDate;
+        public TimeSpan Span;
+        public int Pawn;
+
+        public DateTime EndDate
+        {
+            get
+            {
+                DateTime temp = StartDate + Span;
+                return temp.Date;
+            }
+        }
+
+        public Reader(string name, string surname, int number)
+        {
+            Name = name;
+            Surname = surname;
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{this} ({Number})\nСписок взятой литературы:");
+
+            foreach (var i in Literature)
+                Console.WriteLine($"\t{i}");
+
+            Console.WriteLine($"Дата выдачи: {StartDate}. Срок выдачи: {Span}. Предполагаемая дата возврата: {EndDate}. Сумма залога: {Pawn}");
+        }
+    }
+}
